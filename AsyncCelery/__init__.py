666730a449f2e1cdc14ccbfed8fe6244efa28dfb
Your Name <silvia.homam@gmail.com>
2019-12-24 19:29:50 +0200

    Add celery configuration. Create the thumbnail additing task

---
+++ AsyncCelery/__init__.py
@@ -0,0 +1,5 @@
+# I need to ensure that the previously created and configured
+# celery application gets injected into the Django application when it is ran.
+from .celery import celery_app
+
+__all__ = ('celery_app', )
